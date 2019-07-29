8f7c56a48acc47da1c0762e4417a9bc8e1d2adef
wl00254294 <wl00254294@gmail.com>
2019-07-29 09:40:24 +0800

    2019/7/29 initial

---
+++ src/main/java/com/middle/main/MiddleServerApplication.java
@@ -0,0 +1,13 @@
+package com.middle.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/middle/main/MiddleServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.middle.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiddleServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
